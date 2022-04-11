c60f7aef265b750cd351b0c0316c63fb40b8cee5
YPRAS <YPRAS>
2022-04-11 09:16:58 +0530

    stockscreener

--- stock_screener/urls.py
+++ stock_screener/urls.py
@@ -18,16 +18,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.static import serve
-from django.conf.urls import url
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("stock_app.urls")),
-
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('',include("stock_app.urls")), 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
